d5724949c833f2425c9bc78e393a6c944cbc0ca6
akshita gangrade <akshitagangrade2@gmail.com>
2016-06-11 13:53:07 +0530

    adding form controller

--- grails-app/controllers/learning/management/HelloController.groovy
+++ grails-app/controllers/learning/management/HelloController.groovy
@@ -5,10 +5,25 @@ class HelloController {
     def index() {
         render("hello world")
     }
-    def signup() {
-    }
+    /*def signup() {
+        //to create default map
+    }*/
+    /*customized mapping
+    def register() {
+
+        render(view: "signup")
+    } */
+
+    /* for mappingdef signup() {
+
+        Map x = [firstName: "apoorv" , lastName: "srivastava" , email: "apoorv@gmail.com"]
+        return x
+    } */
+
     def saveUser() {
         println(params)
         render("thankyou for sign-up")
     }
+
+
 }
