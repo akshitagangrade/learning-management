c845aed326bed8361f2acae0f89819a003b4c22f
akshita gangrade <akshitagangrade2@gmail.com>
2016-06-16 00:00:00 +0530

    adding groovy files

---
+++ src/groovy/learning/management/SaveContent.groovy
@@ -0,0 +1,19 @@
+package learning.management
+
+/**
+ * Created by akshita on 13/6/16.
+ */
+class SaveContent {
+    String myfirstName
+    String mylastName
+    String myage
+    int myid
+
+    //to change lower clase to upper case
+    void myTask() {
+        myfirstName = myfirstName.toUpperCase()
+        mylastName = mylastName.toUpperCase()
+    }
+
+}
+
